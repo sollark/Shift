5d975581151623cccb2fc8de895a974c2d62d77c
Andrey Solonikov <andreysol@yahoo.com>
2024-03-24 09:20:35 +0200

    Add NotFoundError class

---
+++ server/src/errors/NotFoundError.ts
@@ -0,0 +1,23 @@
+import CustomError from './CustomError'
+
+class NotFoundError extends CustomError {
+  statusCode = 200
+  errorType = 'NOT_FOUND_ERROR'
+
+  constructor(public message: string, public resourceName?: string) {
+    super(message)
+    this.name = 'NotFoundError'
+    Object.setPrototypeOf(this, NotFoundError.prototype)
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.message,
+        resourceName: this.resourceName,
+      },
+    ]
+  }
+}
+
+export default NotFoundError
