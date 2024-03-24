08710e2f08e00ffa15872aab2ed585bfd4d1fd22
Andrey Solonikov <andreysol@yahoo.com>
2024-03-24 12:32:46 +0200

    Refactor account service

--- server/src/api/account/account.service.ts
+++ server/src/api/account/account.service.ts
@@ -10,7 +10,6 @@ import AccountModel, {
 } from '../../mongo/models/account.model.js'
 import ProfileModel, { Profile } from '../../mongo/models/profile.model.js'
 import logger from '../../service/logger.service.js'
-import { profileService } from '../profile/profile.service.js'
 
 async function createAccount(uuid: string): Promise<Partial<Account>> {
   try {
@@ -22,9 +21,7 @@ async function createAccount(uuid: string): Promise<Partial<Account>> {
     }
 
     // Cast a document to an object
-    const account = (await AccountModel.findById(
-      accountDoc._id
-    ).lean()) as Account
+    const account = accountDoc.toObject() as Account
     if (!account) {
       logger.warn(`accountService - cannot get account: ${accountDoc._id}`)
       throw new NotFoundError('Could not get account')
@@ -85,7 +82,7 @@ async function getAccountDoc(uuid: string): Promise<AccountDoc> {
 async function updateRole(
   accountId: Types.ObjectId,
   role: Role
-): Promise<Account> {
+): Promise<Account & { _id: Types.ObjectId }> {
   try {
     const updatedAccount = await AccountModel.findByIdAndUpdate(
       accountId,
@@ -93,248 +90,122 @@ async function updateRole(
       // { new: true } // returns new version of document, if false returns original version, before updates
       { new: true }
     )
+      .populate<{ role: Role }>('role')
+      .populate<{ profile: Profile }>('profile')
+      .populate<{ status: Status }>('status')
+      .lean()
+      .exec()
     if (!updatedAccount) {
       logger.warn(
         `accountService - updateRole, account is not found: ${accountId}`
       )
       throw new NotFoundError('Account is not found')
     }
-    const account = await updatedAccount.lean().exec()
 
-    return account
+    return updatedAccount
   } catch (error) {
     logger.error(`accountService - updateRole, error: ${error}`)
     throw new InternalServerError('Error updating account')
   }
 }
 
-async function setStatus(accountId: Types.ObjectId, status: Status) {
-  const account = await AccountModel.findByIdAndUpdate(
-    accountId,
-    { status },
-    // returns new version of document, if false returns original version, before updates
-    { new: true }
-  ).exec()
-}
-
-async function connectEmployee(
+async function updateStatus(
   accountId: Types.ObjectId,
-  employeeId: Types.ObjectId
-): Promise<(Account & { _id: Types.ObjectId }) | null> {
-  const profileId = await employeeService.getProfileId(employeeId)
-
-  // TODO: check if employee is already assigned to another account
-  // TODO: check if account has already assigned profile. Can be separate function to set profile
-  const account = await AccountModel.findByIdAndUpdate(
-    accountId,
-    {
-      $set: {
-        employee: employeeId,
-        profile: profileId,
-        isComplete: true,
-      },
-    },
-    // returns new version of document, if false returns original version, before updates
-    { new: true }
-  )
-    .populate<{ role: Role }>('role')
-    .populate<{ profile: Profile }>('profile')
-    .populate<{ employee: Employee }>({
-      path: 'employee',
-      populate: [
-        {
-          path: 'company',
-          select: 'companyName companyNumber',
-        },
-        {
-          path: 'department',
-          select: 'departmentName',
-        },
-        { path: 'profile' },
-        { path: 'supervisor' },
-        { path: 'subordinates' },
-      ],
-    })
-    .populate<{ status: Status }>('status')
-    .lean()
-    .exec()
-
-  return account
-}
-
-async function disconnectEmployee(accountId: Types.ObjectId) {
-  const account = await AccountModel.findByIdAndUpdate(
-    accountId,
-    {
-      $unset: {
-        employee: 1,
-        isComplete: false,
-      },
-    },
-    // returns new version of document, if false returns original version, before updates
-    { new: true }
-  )
-
-  logger.info(`accountService - disconnectEmployee, accountId: ${accountId}`)
-}
-
-async function completeAccount(
-  accountId: Types.ObjectId
-): Promise<(Account & { _id: Types.ObjectId }) | null> {
-  const updatedAccount = await AccountModel.findByIdAndUpdate(
-    accountId,
-    { isComplete: true },
-    // returns new version of document, if false returns original version, before updates
-    { new: true }
-  )
-    .populate<{ role: Role }>('role')
-    .populate<{ profile: Profile }>('profile')
-    .populate<{ employee: Employee }>({
-      path: 'employee',
-      populate: [
-        {
-          path: 'company',
-          select: 'companyName companyNumber',
-        },
-        {
-          path: 'department',
-          select: 'departmentName',
-        },
-        { path: 'profile' },
-        { path: 'supervisor' },
-        { path: 'subordinates' },
-      ],
-    })
-    .populate<{ status: Status }>('status')
-    .lean()
-    .exec()
-
-  if (!updatedAccount) {
-    logger.warn(
-      `accountService - completeAccount, cannot update account: ${accountId}`
+  status: Status
+): Promise<Account & { _id: Types.ObjectId }> {
+  try {
+    const updatedAccount = await AccountModel.findByIdAndUpdate(
+      accountId,
+      { status },
+      // returns new version of document, if false returns original version, before updates
+      { new: true }
     )
-    throw new BadRequestError('Account cannot be updated')
+      .populate<{ role: Role }>('role')
+      .populate<{ profile: Profile }>('profile')
+      .populate<{ status: Status }>('status')
+      .lean()
+      .exec()
+    if (!updatedAccount) {
+      logger.warn(
+        `accountService - updateStatus, account is not found: ${accountId}`
+      )
+      throw new NotFoundError('Account is not found')
+    }
+
+    return updatedAccount
+  } catch (error) {
+    logger.error(`accountService - updateStatus, error: ${error}`)
+    throw new InternalServerError('Error updating account')
   }
-
-  logger.info(
-    `accountService - completeAccount, account is completed: ${updatedAccount._id}`
-  )
-
-  return updatedAccount
 }
 
 async function updateAccount(
   accountId: Types.ObjectId,
   role: Role,
   status: Status
-): Promise<(Account & { _id: Types.ObjectId }) | null> {
-  const account = await AccountModel.findByIdAndUpdate(
-    accountId,
-    { role, status },
-    // returns new version of document, if false returns original version, before updates
-    { new: true }
-  )
-    .populate<{ profile: Profile }>('profile')
-    .populate<{ employee: Employee }>({
-      path: 'employee',
-      select: '-profile -supervisor -subordinates',
-      populate: [
-        {
-          path: 'company',
-          select: 'companyName',
-        },
-        {
-          path: 'department',
-          select: 'departmentName',
-        },
-      ],
-    })
-    .lean()
-    .exec()
+): Promise<Account & { _id: Types.ObjectId }> {
+  try {
+    const updatedAccount = await AccountModel.findByIdAndUpdate(
+      accountId,
+      { role, status },
+      // returns new version of document, if false returns original version, before updates
+      { new: true }
+    )
+      .populate<{ role: Role }>('role')
+      .populate<{ profile: Profile }>('profile')
+      .populate<{ status: Status }>('status')
+      .lean()
+      .exec()
+    if (!updatedAccount) {
+      logger.warn(
+        `accountService - updateAccount, account is not found: ${accountId}`
+      )
+      throw new NotFoundError('Account is not found')
+    }
 
-  logger.info(
-    `accountService - updateAccount, account is updated: ${accountId}`
-  )
+    logger.info(
+      `accountService - updateAccount, account is updated: ${updatedAccount}`
+    )
 
-  return account
+    return updatedAccount
+  } catch (error) {
+    logger.error(`accountService - updateAccount, error: ${error}`)
+    throw new InternalServerError('Error updating account')
+  }
 }
 
-async function deleteAccount(identifier: Types.ObjectId) {
-  await AccountModel.findOneAndDelete({ identifier })
+async function deleteAccount(accountId: Types.ObjectId) {
+  await AccountModel.findByIdAndDelete({ accountId })
 }
 
 function sortAccountData(
   accountData: any
-): [
-  updatedProfileData: Partial<Profile>,
-  updateEmployeeData: Partial<Employee>,
-  updatedCompanyData: Partial<Company>,
-  updatedDepartmentData: Partial<Department>
-] {
-  // function usage:
-  // const [
-  //   updatedProfileData,
-  //   updateEmployeeData,
-  //   updatedCompanyData,
-  //   updatedDepartmentData,
-  // ] = accountService.sortAccountData(accountData)
-
+): [updatedProfileData: Partial<Profile>] {
   const profileSchemaKeys = Object.keys(ProfileModel.schema.paths)
-  const employeeSchemaKeys = Object.keys(EmployeeModel.schema.paths)
-  const companySchemaKeys = Object.keys(CompanyModel.schema.paths)
-  const departmentSchemaKeys = Object.keys(DepartmentModel.schema.paths)
 
-  const {
-    updatedProfileData,
-    updateEmployeeData,
-    updatedCompanyData,
-    updatedDepartmentData,
-  } = Object.entries(accountData).reduce(
+  const { updatedProfileData } = Object.entries(accountData).reduce(
     (accumulator: any, [key, value]) => {
       if (profileSchemaKeys.includes(key)) {
         accumulator.updatedProfileData[key] = value
-      } else if (employeeSchemaKeys.includes(key)) {
-        accumulator.updateEmployeeData[key] = value
-      } else if (companySchemaKeys.includes(key)) {
-        accumulator.updatedCompanyData[key] = value
-      } else if (departmentSchemaKeys.includes(key)) {
-        accumulator.updatedDepartmentData[key] = value
       }
       return accumulator
     },
     {
       updatedProfileData: {},
-      updateEmployeeData: {},
-      updatedCompanyData: {},
-      updatedDepartmentData: {},
     }
   )
 
   console.log('sortAccountData, updatedProfileData: ', updatedProfileData)
-  console.log('sortAccountData, updateEmployeeData: ', updateEmployeeData)
-  console.log('sortAccountData, updatedCompanyData: ', updatedCompanyData)
-  console.log('sortAccountData, updatedDepartmentData: ', updatedDepartmentData)
 
-  return [
-    updatedProfileData as Partial<Profile>,
-    updateEmployeeData as Partial<Employee>,
-    updatedCompanyData as Partial<Company>,
-    updatedDepartmentData as Partial<Department>,
-  ]
+  return [updatedProfileData as Partial<Profile>]
 }
 
 export const accountService = {
   createAccount,
   getAccount,
-  getAccounts,
   getAccountDoc,
-  getEmployeeAccountDoc,
-  setProfile,
   updateRole,
-  setStatus,
-  connectEmployee,
-  disconnectEmployee,
-  completeAccount,
+  updateStatus,
   updateAccount,
   deleteAccount,
   sortAccountData,
